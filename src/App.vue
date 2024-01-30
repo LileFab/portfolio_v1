28cc037c64462f3e661b0324bbcf06da56ae4aa2
LileFab <backspeen@gmail.com>
2024-01-30 22:28:52 +0100

    ajout CV et changement icone

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,9 @@ import Header from './components/Header.vue';
     <i class="animate-spin text-indigo-500 fas fa-spinner fa-lg"></i>
       <span class="text-2xl ml-3">Work In Progress</span>
     </div>
+     <a href="@/assets/pdf/document.pdf" download="CV_Fabien_Fleisch.pdf" class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-baby_powder bg-cinereous-500 hover:bg-cinereous-400 ml-20">
+        <i class="fas fa-download mr-2"></i> Télécharger mon CV
+      </a>
     
   </div>
 </template>
