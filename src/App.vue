9774d1e8996aa908e690a6600d59cf1646603616
LileFab <backspeen@gmail.com>
2024-01-30 22:18:25 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div class="bg-baby_powder h-screen mt-20 scroll-smooth font-roboto">
+    
+      <Header />
+    <div id="main" class="p-20 pt-4">
+      <RouterView />
+    <i class="animate-spin text-indigo-500 fas fa-spinner fa-lg"></i>
+      <span class="text-2xl ml-3">Work In Progress</span>
+    </div>
+    
+  </div>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-gradient-to-b from-cambridge_blue-700 to-baby_powder h-20 flex flex-row fixed top-0 w-screen">
+    <img src="@\assets\svg\tasse.svg" alt="">
+    <span class="text-2xl mr-10 flex items-center justify-center pl-3 font-workbench animate-pulse">Fabien</span>
+    <ul>
+      <li><a href=""></a></li>
+      <li><a href=""></a></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+  name: 'Header'
+};
+</script>
