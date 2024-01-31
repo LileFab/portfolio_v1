b0b3028981a866183d2cad657171e62cccc2047e
LileFab <backspeen@gmail.com>
2024-01-31 16:03:34 +0100

    icone tourne

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,15 @@
 
 <template>
   <div class="bg-baby_powder h-screen scroll-smooth font-roboto">
-    <header class="h-16 bg-gradient-to-r from-cambridge_blue  to-jet opacity-50">
+    <header class="h-16 bg-gradient-to-r from-cambridge_blue  to-jet opacity-95 top-0 fixed w-screen">
       <div class="opacity-100">
-        <i class="fa-solid fa-otter text-4xl p-3"></i>
+        <i class="fa-solid fa-otter text-4xl p-3 animate-waving-hand"></i>
         <a href="" class="font-bold text-3xl font-playfair opacity-90"> Fabien Fleisch</a>
       </div>
     </header>
-    <div id="main" class="p-20 pt-4">
+    <div id="main" class="p-20 pt-36">
         <h1 class="font-bold text-5xl md:text-7xl text-center mt-6 font-playfairb bg-gradient-to-r from-cambridge_blue  to-jet text-transparent bg-clip-text">Fabien's Portfolio</h1>
 
-      
     </div>
      <!-- <a href="@/assets/pdf/document.pdf" download="CV_Fabien_Fleisch.pdf" class="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-baby_powder bg-cinereous-500 hover:bg-cinereous-400 ml-20">
         <i class="fas fa-download mr-2"></i> Télécharger mon CV
--- tailwind.config.js
+++ tailwind.config.js
@@ -125,6 +125,24 @@ export default {
 					900: '#efe3dd',
 				},
 			},
+			keyframes: {
+				wave: {
+					'0%': { transform: 'rotate(0.0deg)' },
+					'10%': { transform: 'rotate(36deg)' },
+					'20%': { transform: 'rotate(72deg)' },
+					'30%': { transform: 'rotate(108deg)' },
+					'40%': { transform: 'rotate(144deg)' },
+					'50%': { transform: 'rotate(180deg)' },
+					'60%': { transform: 'rotate(216deg)' },
+					'70%': { transform: 'rotate(252deg)' },
+					'80%': { transform: 'rotate(288deg)' },
+					'90%': { transform: 'rotate(324deg)' },
+					'100%': { transform: 'rotate(360deg)' },
+				},
+			},
+			animation: {
+				'waving-hand': 'wave 60s linear infinite',
+			},
 		},
 		fontFamily: {
 			roboto: ['Roboto', 'sans-serif'],
