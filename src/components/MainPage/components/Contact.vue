57c83080dfdbabc90197cee25bc00700e7c26623
Fabien <backspeen@gmail.com>
2024-05-29 17:52:22 +0200

    Update Contact.vue

--- src/components/MainPage/components/Contact.vue
+++ src/components/MainPage/components/Contact.vue
@@ -3,7 +3,7 @@
     <h2 class="font-bold text-4xl text-center font-playfairb text-brown_sugar pb-10 mt-28">Contact me</h2>
     <div class="grid grid-cols-3 gap-6 text-center">
       <div class="col-span-2 text-justify pr-10 pl-10 space-y-4">
-        <p class="text-xl font-roboto pt-16 pl-8"><i class="fa-solid fa-paper-plane pr-2"></i>Ecrivez-moi : <span class="text-brown_sugar">fleischfabien@gmail.com</span></p>
+        <p class="text-xl font-roboto pt-16 pl-8"><i class="fa-solid fa-paper-plane pr-2"></i>Ecrivez-moi : <span class="text-brown_sugar">fleischfabien[at]gmail.com</span></p>
       </div>
     <div>
       <img src="@\assets\svg\undraw_personal_information_re_vw8a.svg" alt="">
